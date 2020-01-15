c47d130d29e155450f3d62f16c9efdd441432342
lacour_a <alexandre.lacour3@hotmail.fr>
2020-01-15 21:46:31 +0100

    model A can be trained

--- main.py
+++ main.py
@@ -5,19 +5,12 @@ import attributes_model
 import pickle
 
 
-def main():
+def train_attributes_model(epochs=10, batch_size=32):
     (X_train, y_train), (X_test, y_test) = data.load_data()
+    print("ATTRIBUTES DATA LOADED.")
 
     X_train = X_train / 255.0
     X_test = X_test / 255.0
-
-    input_shape = X_train.shape[1:4]
-    num_attributes = y_train.shape[1]
-    batch_size = 32
-
-    checkpointer = ModelCheckpoint('cnn_re_id_weights.h5',
-                                   save_best_only=True,
-                                   monitor='val_loss')
 
     train_datagen = ImageDataGenerator()
     test_datagen = ImageDataGenerator()
@@ -27,17 +20,34 @@ def main():
     test_generator = test_datagen.flow(X_test, y_test,
                                        batch_size=batch_size)
 
+    print("ATTRIBUTES GENERATORS CREATED.")
+
+    input_shape = X_train.shape[1:4]
+    num_attributes = y_train.shape[1]
+
     model = attributes_model.create_model(input_shape, num_attributes)
+
+    checkpointer = ModelCheckpoint('attributes_model.h5',
+                                   save_best_only=True,
+                                   monitor='val_loss')
+
+    print("ATTRIBUTES MODEL CREATED.")
 
     h = model.fit_generator(train_generator,
                             steps_per_epoch=len(X_train) // batch_size,
                             validation_data=test_generator,
                             validation_steps=len(X_test) // batch_size,
-                            epochs=100,
+                            epochs=epochs,
                             callbacks=[checkpointer])
 
-    with open('history_cnn_re_id.pickle', 'wb') as file:
+    print("ATTRIBUTES MODEL : TRAINING OVER.")
+
+    with open('history_attributes_model.pickle', 'wb') as file:
         pickle.dump(h.history, file)
+
+
+def main():
+    train_attributes_model(epochs=5)
 
 
 if __name__ == '__main__':
